89c822a5e0f0d3c5ca6f913c29a535bfd120c6a8
davidenrique <davidenriquea@icloud.com>
2025-10-18 14:22:32 +0200

    Variables numero y cadenas inicializadas y pedidas por consola.

---
+++ Ejercicio1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+
+
+int main(){
+    int numero;
+    char cadena1[100], cadena2[100];
+
+    printf("Input a number: ");
+    scanf("%d", numero);
+
+    printf("Input first string: ");
+    scanf("%s", cadena1);
+
+    printf("Input second string: ");
+    scanf("%s", cadena2);
+
+}
