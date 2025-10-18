666c223a040c65edb44bf0cbb5e24f04bf4a47bb
davidenrique <davidenriquea@icloud.com>
2025-10-18 14:25:03 +0200

    Hilos inicializados y parametrso del hilo asignados.

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -3,6 +3,10 @@
 #include <pthread.h>
 #include <string.h>
 
+typedef struct {
+    char cad[100];
+    int num;
+} Parametros;
 
 
 int main(){
@@ -17,5 +21,14 @@ int main(){
 
     printf("Input second string: ");
     scanf("%s", cadena2);
+
+    pthread_t hilo1, hilo2;
+    Parametros p1, p2;
+
+    strcpy(p1.cad, cadena1);
+    p1.num = numero;
+
+    strcpy(p2.cad, cadena2);
+    p2.num = numero;
 
 }
